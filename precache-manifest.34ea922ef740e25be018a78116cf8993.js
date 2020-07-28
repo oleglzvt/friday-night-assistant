751f7dd30db930e0480c1ffaab41c462d6cf4e9e
Oleg Lozovatyi <lozovatyi.jr@gmail.com>
2020-07-28 15:27:05 -0400

    Updates

---
+++ precache-manifest.34ea922ef740e25be018a78116cf8993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b830a0a77a796eb057afcd2e8af3bbc7",
+    "url": "/friday-night-assistant/index.html"
+  },
+  {
+    "revision": "31d2c794c5398674aeb1",
+    "url": "/friday-night-assistant/static/css/main.faf4b8ce.chunk.css"
+  },
+  {
+    "revision": "6b85858f12c247374ccc",
+    "url": "/friday-night-assistant/static/js/2.6794138a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friday-night-assistant/static/js/2.6794138a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d2c794c5398674aeb1",
+    "url": "/friday-night-assistant/static/js/main.d78fd17c.chunk.js"
+  },
+  {
+    "revision": "702f97d56de779244829",
+    "url": "/friday-night-assistant/static/js/runtime-main.938cb06b.js"
+  }
+]);
