318c209280174d8a4cc695a6952976e3e3c535d3
Oleg Lozovatyi <lozovatyi.jr@gmail.com>
2020-07-28 15:56:14 -0400

    fixed ingredients display

--- src/NameSearch.js
+++ src/NameSearch.js
@@ -3,22 +3,22 @@ import './App.css';
 
 class NameSearch extends Component {
 
-	constructor() {
-		super();
-		this.state = {
-			plusButtonShown: true,
-			minusButtonShown: false,
-			recipeShown: false,
-		}
-	}
+	// constructor() {
+	// 	super();
+	// 	this.state = {
+	// 		plusButtonShown: true,
+	// 		minusButtonShown: false,
+	// 		recipeShown: false,
+	// 	}
+	// }
 
-	handleToggle = () => {
-		this.setState({
-			plusButtonShown: this.state.plusButtonShown ? false : true,
-			minusButtonShown: this.state.minusButtonShown ? false : true,
-			testResultsShown: this.state.recipeShown ? false : true
-		})
-	}
+	// handleToggle = () => {
+	// 	this.setState({
+	// 		plusButtonShown: this.state.plusButtonShown ? false : true,
+	// 		minusButtonShown: this.state.minusButtonShown ? false : true,
+	// 		testResultsShown: this.state.recipeShown ? false : true
+	// 	})
+	// }
 	
 
 	render() {
@@ -40,12 +40,12 @@ class NameSearch extends Component {
 									<h2>{drink.strDrink}</h2>
 									<p>You will need:</p>
 									<ul className="ingredients">
-										<li>{drink.strIngredient1 ? drink.strIngredient1 : null}: {drink.strMeasure1}</li>
-										<li>{drink.strIngredient2 ? drink.strIngredient2 : null}: {drink.strMeasure2}</li>
-										<li>{drink.strIngredient3 ? drink.strIngredient3 : null}: {drink.strMeasure3}</li>
-										<li>{drink.strIngredient4 ? drink.strIngredient4 : null}: {drink.strMeasure4}</li>
-										<li>{drink.strIngredient5 ? drink.strIngredient5 : null}: {drink.strMeasure5}</li>
-										<li>{drink.strIngredient6 ? drink.strIngredient6 : null}: {drink.strMeasure6}</li>
+										<li>{drink.strIngredient1 ? drink.strIngredient1 : null}{drink.strMeasure1 ? `: ${drink.strMeasure1}` : null}</li>
+                                        <li>{drink.strIngredient2 ? drink.strIngredient2 : null}{drink.strMeasure2 ? `: ${drink.strMeasure2}` : null}</li>
+                                        <li>{drink.strIngredient3 ? drink.strIngredient3 : null}{drink.strMeasure3 ? `: ${drink.strMeasure3}` : null}</li>
+                                        <li>{drink.strIngredient4 ? drink.strIngredient4 : null}{drink.strMeasure4 ? `: ${drink.strMeasure4}` : null}</li>
+                                        <li>{drink.strIngredient5 ? drink.strIngredient5 : null}{drink.strMeasure5 ? `: ${drink.strMeasure5}` : null}</li>
+                                        <li>{drink.strIngredient6 ? drink.strIngredient6 : null}{drink.strMeasure6 ? `: ${drink.strMeasure6}` : null}</li>
 									</ul>
 									<p>{drink.strInstructions}</p>
 								</div>
@@ -64,8 +64,8 @@ class NameSearch extends Component {
 			)
 		} else {
 			return(
-				<div>
-					<h2 className="errorDescription">Unfortunately, I don't recognize this drink :(</h2>
+				<div className="errorDescription">
+					<h2>Unfortunately, I don't recognize this drink :(</h2>
 					<button onClick={this.props.refreshPage}>Try another one</button>
 				</div>
 			)
--- src/RandomSearch.js
+++ src/RandomSearch.js
@@ -16,12 +16,20 @@ class RandomSearch extends Component {
                                     <h2>{drink.strDrink}</h2>
                                     <p>You will need:</p>
                                     <ul className="ingredients">
-                                        <li>{drink.strIngredient1 ? drink.strIngredient1 : null}: {drink.strMeasure1}</li>
-                                        <li>{drink.strIngredient2 ? drink.strIngredient2 : null}: {drink.strMeasure2}</li>
+                                        <li>{drink.strIngredient1 ? drink.strIngredient1 : null}{drink.strMeasure1 ? `: ${drink.strMeasure1}` : null}</li>
+                                        <li>{drink.strIngredient2 ? drink.strIngredient2 : null}{drink.strMeasure2 ? `: ${drink.strMeasure2}` : null}</li>
+                                        <li>{drink.strIngredient3 ? drink.strIngredient3 : null}{drink.strMeasure3 ? `: ${drink.strMeasure3}` : null}</li>
+                                        <li>{drink.strIngredient4 ? drink.strIngredient4 : null}{drink.strMeasure4 ? `: ${drink.strMeasure4}` : null}</li>
+                                        <li>{drink.strIngredient5 ? drink.strIngredient5 : null}{drink.strMeasure5 ? `: ${drink.strMeasure5}` : null}</li>
+                                        <li>{drink.strIngredient6 ? drink.strIngredient6 : null}{drink.strMeasure6 ? `: ${drink.strMeasure6}` : null}</li>
+                                        
+                                        {/* <li>{drink.strIngredient2 ? drink.strIngredient2 : null}: {drink.strMeasure2}</li>
                                         <li>{drink.strIngredient3 ? drink.strIngredient3 : null}: {drink.strMeasure3}</li>
                                         <li>{drink.strIngredient4 ? drink.strIngredient4 : null}: {drink.strMeasure4}</li>
-                                        <li>{drink.strIngredient5 ? drink.strIngredient5 : null}: {drink.strMeasure5}</li>
-                                        <li>{drink.strIngredient6 ? drink.strIngredient6 : null}: {drink.strMeasure6}</li>
+                                        <li>{drink.strIngredient5 ? `${drink.strIngredient5}: ` : null}{drink.strMeasure5}</li>
+                                        <li>{drink.strIngredient6 ? `${drink.strIngredient6}: ` : null}{drink.strMeasure6}</li> */}
+                                        {/* <li>{drink.strIngredient5 ? drink.strIngredient5 : null}: {drink.strMeasure5}</li>
+                                        <li>{drink.strIngredient6 ? drink.strIngredient6 : null}: {drink.strMeasure6}</li> */}
                                         {/* <li>{drink.strIngredient1}</li>
                                         <li>{drink.strIngredient2}</li>
                                         <li>{drink.strIngredient3}</li>
